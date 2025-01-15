1ae50774cb89ee7b875f9f99b02d8b38341e52b3
Victim726 <captaincook726@gmail.com>
2025-01-14 19:33:11 -0500

    cleaned up imports

--- src/client/java/gay/plat/victeemtweaks/VicteemTweaks.java
+++ src/client/java/gay/plat/victeemtweaks/VicteemTweaks.java
@@ -3,20 +3,15 @@ package gay.plat.victeemtweaks;
 import com.mojang.brigadier.arguments.BoolArgumentType;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import gay.plat.victeemtweaks.itemrenamer.ItemNameManager;
-import gay.plat.victeemtweaks.screens.ItemRenamerListScreen;
 import gay.plat.victeemtweaks.util.PlayerUtil;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
-import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
-import net.fabricmc.fabric.api.event.player.UseItemCallback;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.command.argument.ItemStackArgumentType;
-import net.minecraft.item.*;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.TypedActionResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -26,8 +21,6 @@ public class VicteemTweaks implements ClientModInitializer {
 	public static final String MODID = "victeemtweaks";
 
 	public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
-
-	public static boolean cooldownTricked = false;
 
 	@Override
 	public void onInitializeClient() {
--- src/client/java/gay/plat/victeemtweaks/mixin/PlayerEntityMixin.java
+++ src/client/java/gay/plat/victeemtweaks/mixin/PlayerEntityMixin.java
@@ -1,6 +1,5 @@
 package gay.plat.victeemtweaks.mixin;
 
-import gay.plat.victeemtweaks.VicteemTweaks;
 import gay.plat.victeemtweaks.itemrenamer.ItemNameManager;
 import net.minecraft.component.DataComponentTypes;
 import net.minecraft.component.type.AttributeModifiersComponent;
